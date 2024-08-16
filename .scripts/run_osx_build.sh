b3d58e3ac63c436195fb48d0f0f7d8846cc013c6
nichmor <nmorkotilo@gmail.com>
2024-08-16 19:02:17 +0300

    fix: wrong spacing

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -69,7 +69,7 @@ else
 
     rattler-build build --recipe ./recipe \
         -m ./.ci_support/${CONFIG}.yaml \
-        --output-dir ${MINIFORGE_HOME}/conda-bld ${EXTRA_CB_OPTIONS:-}
+        --output-dir ${MINIFORGE_HOME}/conda-bld ${EXTRA_CB_OPTIONS:-} \
         --extra-meta flow_run_id="$flow_run_id" \
         --extra-meta remote_url="$remote_url" \
         --extra-meta sha="$sha"
